7475e3c57999c1c7b5ae9945ce131984e1739cfd
matioli_senai <guilhermemsbr@gmail.com>
2025-11-19 16:48:24 -0300

    feat: Initial setup of login and register

--- appfleshi/routes.py
+++ appfleshi/routes.py
@@ -1,14 +1,18 @@
 from flask import  render_template, request
 from flask_login import login_required
-
 from appfleshi import app
+from appfleshi.forms import LoginForm, RegisterForm
 
 #redireciona p login
 @app.route('/')
 def homepage():
-    return render_template('homepage.html')
+    login_form = LoginForm()
+    return render_template('homepage.html', form=login_form)
 
-
+@app.route("/createaccount", methods=['GET', 'POST'])
+def createaccount():
+    register_form = RegisterForm()
+    return render_template('createaccount.html')
 
 #rota dinamica nome da var aq eh o perfil
 @app.route('/profile/<username>')
