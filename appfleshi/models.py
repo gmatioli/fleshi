826d972ac4758e8447287eb78954fdfbbd359d66
matioli_senai <guilhermemsbr@gmail.com>
2025-11-19 15:00:09 -0300

    feat: Login and security system setup

--- appfleshi/models.py
+++ appfleshi/models.py
@@ -1,22 +1,26 @@
 from zoneinfo import ZoneInfo
-from datetime import datetime
+
+from datetime import  datetime
+
 from appfleshi import database, login_manager
 from flask_login import UserMixin
 
+#add o userMixin para saber q eh daq q vem os logins
+class User(database.Model, UserMixin):
+  id = database.Column(database.Integer, primary_key=True)
+  username = database.Column(database.String(20), unique=True, nullable=False)
+  email = database.Column(database.String(100), unique=True, nullable=False)
+  password = database.Column(database.String(60), nullable=False)
+  photos = database.relationship('Photo', backref='user', lazy=True)
+
+#função p carregar o user (pega pelo id)
+#sempre q altera algo no models roda o create_database p atualizar
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-class User(database.Model, UserMixin):
-    id = database.Column(database.Integer, primary_key=True)
-    username = database.Column(database.String(20), unique=True, nullable=False)
-    email = database.Column(database.String(100), unique=True, nullable=False)
-    password = database.Column(database.String(60), nullable=False)
-    photos = database.relationship("Photo", backref="user", lazy=True)
-    pass
-
 class Photo(database.Model):
     id = database.Column(database.Integer, primary_key=True)
-    user_id = database.Column(database.Integer, database.ForeignKey("user.id"), nullable=False)
     file_name = database.Column(database.String(255), unique=True, nullable=False, default='default.png')
     upload_date = database.Column(database.DateTime, nullable=False, default=lambda: datetime.now(ZoneInfo("America/Sao_Paulo")))
+    user_id = database.Column(database.Integer, database.ForeignKey('user.id'), nullable=False)
--- appfleshi/routes.py
+++ appfleshi/routes.py
@@ -1,10 +1,18 @@
-from flask import render_template
-from  appfleshi import app
+from flask import  render_template, request
+from flask_login import login_required
 
+from appfleshi import app
+
+#redireciona p login
 @app.route('/')
 def homepage():
     return render_template('homepage.html')
 
-@app.route("/profile/<username>")
+
+
+#rota dinamica nome da var aq eh o perfil
+@app.route('/profile/<username>')
+@login_required
 def profile(username):
     return render_template('profile.html', username=username)
+
