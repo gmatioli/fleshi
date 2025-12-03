1b779623e0e1195b1a37a6842e6a16fe3d0238c5
matioli_senai <guilhermemsbr@gmail.com>
2025-12-03 16:17:56 -0300

    feat: Add feed page and navbar

--- appfleshi/routes.py
+++ appfleshi/routes.py
@@ -53,4 +53,10 @@ def logout():
     logout_user()
     return redirect(url_for('homepage'))
 
+@app.route('/feed')
+@login_required
+def feed():
+    photos = Photo.query.order_by(Photo.upload_date.desc()).all()
+    return render_template('feed.html', photos=photos)
+
 
